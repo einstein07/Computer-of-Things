c9a2a17f09dd8a1f3b02beb3828afe8a8fa3573c
einstein07 <mkhsin035@myuct.ac.za>
2022-07-07 09:50:17 +0200

    Added preliminary code for wrapper function to process_work-packages function call

--- client-webapp/computer-of-things/src/App.js
+++ client-webapp/computer-of-things/src/App.js
@@ -1,6 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
 import Module from "./ComputerOfThings.mjs";
+// Webassembly: shared objects?
+// https://stackoverflow.com/questions/67655485/webassembly-possible-to-have-shared-objects
+/**
+* @param {Module}
+* @return {Array} - an array of fft values
+**/
+function wrapProcessWorkPackages(Module) {
+  // JS-friendly wrapper around the WASM call
+  return function (inputData) {
+    const length = inputData.length;
+
+    // set up input arrays with the input data -- remove holes if any
+    const flatInputData = new Float32Array(inputData);
+
+    const buffer = Module._malloc(
+      flatInputData.length * flatInputData.BYTES_PER_ELEMENT
+    );
+    Module.HEAPF32.set(flatInputData, buffer >> 2);
+
+    // allocate memory for the result array
+    const resultBuffer = Module._malloc(
+      flatInputData.length * flatInputData.BYTES_PER_ELEMENT
+    );
+
+    // make the call
+    const resultPointer = Module.ccall(
+      "process_work_packages",
+      "number",
+      ["number", "number"],
+      [buffer, resultBuffer]
+    );
+
+    // get the data from the returned pointer into an flat array
+    const result = [];
+    for (let i = 0; i < length; i++) {
+      result.push(
+        Module.HEAPF32[resultPointer / Float32Array.BYTES_PER_ELEMENT + i]
+      );
+    }
+    // Release memory
+    Module._free(buffer);
+    Module._free(resultBuffer);
+    return result;
+  };
+}
+
 
 function App() {
 
