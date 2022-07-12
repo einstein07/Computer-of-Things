f3eead43153f7837ac3caa947a10c7fc2af5232f
Malan Ruppert <malan.ruppert@vastech.co.za>
2022-07-12 09:53:15 +0200

    server: updated ws server to continuously send a new workpackage every 3 seconds

--- websocket-server/src/computer_of_things/server/__main__.py
+++ websocket-server/src/computer_of_things/server/__main__.py
@@ -3,23 +3,36 @@ import websockets
 from .utils import createMatrixMultiplyRequest
 from websockets.connection import Connection
 
-
-# Use a global list to keep track of connected clients, and whether they're busy
-CONNECTED_CLIENTS = []
-
 async def connection_handler(websocket_connection: Connection):
     """
     Entry point handler that takes care of connections from clients
-
     """
+    await asyncio.gather(
+        client_response_handler(websocket_connection),
+        work_package_producer(websocket_connection)
+    )
 
-    CONNECTED_CLIENTS.append(websocket_connection)
-    print("request byte array:")
-    buff = createMatrixMultiplyRequest(2, 2)
-    print(", ".join(hex(b) for b in buff))
-    await websocket_connection.send(buff)
-    response = await websocket_connection.recv()
-    print("Received response from client: ", response)
+async def client_response_handler(websocket):
+        # Iteration terminates when the client disconnects.
+        async for message in websocket:
+            # each message may be a str or bytes
+            process_response(message)
+
+def process_response(message):
+    print("Received response from client: {}".format(message))
+
+async def work_package_producer(websocket):
+    while True:
+        # let's send a new workpackage every 3 seconds
+        await asyncio.sleep(3)
+        message = get_next_work_package()
+        await websocket.send(message)
+
+def get_next_work_package():
+    """
+    Simple method to simulate the server getting new workpackages
+    """
+    return createMatrixMultiplyRequest(2, 2)
 
 
 
